ee86790ff2049e869b4790a043fd92d7a51e0738
Ron Hansen <ron.hansen112@gmail.com>
2017-03-20 12:23:05 +0100

    Lebenslauf geändert und favicon

--- src/components/schulzeugnis.vue
+++ src/components/schulzeugnis.vue
@@ -2,7 +2,7 @@
     <div class="notification is-primary">
         <div class="column is-offset-4">
             <a class="button is-primary is-inverted is-outlined"
-               href="../file.pdf"><i
+               href="../berufsschulzeugnis.pdf"><i
                     class="fa fa-file-pdf-o" aria-hidden="true"></i>&nbsp;Letztes Schulzeugnis herunterladen</a>
         </div>
     </div>
